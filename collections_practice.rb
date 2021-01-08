b49c7ebea71746e487060fa3203150f4733c0b36
Sara Nolen <nolensara1@gmail.com>
2021-01-08 15:45:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ def kesha_maker(array)
 
     i = i + 1
   end
- array[3] = "$"
+ array
 end
 
-kesha_maker(["blake", "ashley", "scott"]) {|name| "name"}
+kesha_maker(["blake", "ashley", "scott"]) {|name| name[3] = "$"}
