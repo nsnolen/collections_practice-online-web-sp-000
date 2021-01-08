4bf592eba39e30ffe0090b1fd0bd870f5493f4cf
Sara Nolen <nolensara1@gmail.com>
2021-01-08 14:46:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+
+def sort_array_char_count(array)
+  array.sort{|a, b| a.downcase <=> b.downcase}
+
+end
+
+
+def swap_elements(array)
+  array[1], array [2] = array [0], array[2], array [1]
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  i = 0
+  tictoc = []
+  while i < array.length
+    tictoc << yield(array[i])
+    i = i + 1
+  end
+ tictoc[3] = "$"
+end
+kesha = ["blake", "ashley", "scott"]
+kesha_maker(kesha){|name| "#{name}"}
